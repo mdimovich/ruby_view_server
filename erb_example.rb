621be0bef6622d5f85171df0edfc85473008a27c
Michael Dimovich <mdimovich@gmail.com>
2013-06-24 12:15:46 -0400

    homework section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,17 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+template = ERB.new "10 + 10 is equal to <%=20 %>"
+puts template.result(binding)
+
+dog_name = "Patches"
+template= ERB.new "The dog's name is <%= dog_name %>"
+puts template.result(binding)
+
+languages = ["HTML", "CSS", "Javascript"]
+iterate = languages.each do |lang|
+			puts "The language is: #{lang}"
+		end
+		template = ERB.new "<%= iterate %>"
+		puts template.result(binding)
